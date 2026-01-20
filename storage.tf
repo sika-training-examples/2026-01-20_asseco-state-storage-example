25a3dade99ae20fc3a7504a876050de69dab4848
Ondrej Sika <ondrej@ondrejsika.com>
2026-01-20 13:10:12 +0000

    feat: Add outputs

--- storage.tf
+++ storage.tf
@@ -18,3 +18,11 @@ resource "azurerm_storage_container" "terraform-states" {
   storage_account_id    = azurerm_storage_account.terraform-states.id
   container_access_type = "private"
 }
+
+output "storage_account_name" {
+  value = azurerm_storage_account.terraform-states.name
+}
+
+output "storage_container_name" {
+  value = azurerm_storage_container.terraform-states.name
+}
